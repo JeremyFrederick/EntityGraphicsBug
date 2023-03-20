89033350e98c44c32a167ad4565896120f9850ee
Jeremy Frederick <jrf407@gmail.com>
2023-03-20 11:56:41 -0400

    Removed unused files. Added help text to scene.

--- EntityGraphicsBug/Assets/SimpleMoveSystem.cs
+++ EntityGraphicsBug/Assets/SimpleMoveSystem.cs
@@ -16,9 +16,9 @@ public partial struct SimpleMoveSystem : ISystem
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        //positionLookup.Update(ref state);
-        //starshipMovePropertiesLookup.Update(ref state);
-
+        // COMMENT OUT THIS ENTIRE FUNCTION AND THE BUG WILL NOT HAPPEN
+        // Actually, it is the ref LocalTransform localTransform in the Execute that causes this issue. If any other component
+        // is used instead of LocalTransform, memory will be allocated on each spawn but it will not continue to grow until it crashes
         float deltaTime = SystemAPI.Time.DeltaTime;
         if (deltaTime > 0f)
         {
